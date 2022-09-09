560cc1fa8d968b0537db1e39bc052d173230ecd3
Parksolmi <7548ck@naver.com>
2022-09-10 01:03:59 +0900

    상수와 변수

---
+++ week2_var.php
@@ -0,0 +1,18 @@
+<?php
+    $a = 10;
+    $b = 20;
+    $c = $a + $b;
+    echo "$a + $b = $c <br>";
+
+    $a = 18.5;
+    $b = 37.3;
+    $c = $a + $b;
+    echo "$a + $b = $c <br>";
+
+    $fruit = "사과";
+    echo "$fruit <br>";
+
+    $fruit = "오렌지";
+    echo "$fruit <br>";
+
+?>
