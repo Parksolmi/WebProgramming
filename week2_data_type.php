5accc0f8f7b5c6b4e5a3bc2a5b88c65fe61a8617
Parksolmi <7548ck@naver.com>
2022-09-11 23:44:59 +0900

    기본데이터 타입

---
+++ week2_data_type.php
@@ -0,0 +1,30 @@
+<?php
+    //정수와 실수
+    $a = 3769;
+    echo "\$a : $a";
+    echo "<br>";
+
+    $a = 126.7;
+    echo "\$a : $a";
+
+    //문자열
+    $title = "<h3>연락처</h3>";
+	$name = "홍길동";
+	$address = "경기도 성남시 분당구";
+	$phone = "010-1234-5678";
+	$email = "user@codingschool.biz";
+
+	echo $title;
+	echo "이름 : $name<br>";
+	echo "주소: $address<br>";
+	echo "전화번호 : $phone<br>";
+	echo "이메일 : $email<br>";
+
+    //불 데이터형 
+    $a = true;
+    $b = false;
+
+    echo $a;
+    echo "<br>";
+    echo $b;
+?>
