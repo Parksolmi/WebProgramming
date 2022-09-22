3326c14e56c29458ba4870b1a0647c47ad927ae0
Parksolmi <7548ck@naver.com>
2022-09-22 13:05:28 +0900

    파일 정리

---
+++ Practice/Week4/view2.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset='utf-8'>
+    </head>
+    <body>
+        <?php
+            $id = $_POST['id'];
+            $password = $_POST['password'];
+        ?>
+
+        <ul>
+            <li>아&nbsp;이&nbsp;디 : <?=$id?> </li>
+            <li>비밀번호 : <?=$password?> </li>
+        </ul>
+    </body>
+</html>
