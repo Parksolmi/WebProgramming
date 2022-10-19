acf171d6db80528cfd7876ad968d4bf219de6cb7
Parksolmi <7548ck@naver.com>
2022-10-19 21:05:53 +0900

    7주차 과제-8장 연습문제

---
+++ Assignment/Week7/5.php
@@ -0,0 +1,17 @@
+<?php
+
+    setcookie("userid","rubato");
+    setcookie("username","김채린");
+    
+    if(isset($_COOKIE["userid"]) && isset($_COOKIE["username"]))
+    {
+        $userid = $_COOKIE["userid"];
+        $username = $_COOKIE["username"];
+
+        echo "userid 쿠키 : ".$userid."<br>";
+        echo "username 쿠키 : ".$username."<br>";
+    }
+    else {
+        echo "쿠키가 생성되지 않았다!";
+    }
+?>
